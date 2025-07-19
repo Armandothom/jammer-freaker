c2ae7ef983644c8d65be17ad5b7e42baa913275b
Armando <armandothomazini@gmail.com>
2025-07-19 15:59:11 -0300

    chore: changed proportions

--- src/game/world/world-tilemap-manager.ts
+++ src/game/world/world-tilemap-manager.ts
@@ -6,8 +6,8 @@ import { TilemapTile } from './types/tilemap-tile.js';
 import { SpriteManager } from '../asset-manager/sprite-manager.js';
 
 export class WorldTilemapManager {
-  public _maxNumberTilesX = 8;
-  public _maxNumberTilesY = 8;
+  public _maxNumberTilesX = 200;
+  public _maxNumberTilesY = 200;
   public _generatedWalls: { x: number, y: number }[] = []
   private readonly noiseValueTreshhold = 0.88;
   private readonly _tilemapSpritesheetName = SpriteSheetName.TERRAIN;
